7ce2c4ce630910294e50c3139326bc732fb69ec0
gheorghesurdu <78215059+gheorghesurdu@users.noreply.github.com>
2021-01-29 14:17:16 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,49 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    SHIP.move(-1)
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    SHOOT = game.createSprite(SHIP.get(LedSpriteProperty.X), SHIP.get(LedSpriteProperty.Y))
+    SHOOT.change(LedSpriteProperty.Brightness, 80)
+    for (let index = 0; index < 4; index++) {
+        SHOOT.change(LedSpriteProperty.Y, -1)
+        basic.pause(150)
+        if (SHOOT.isTouching(ENEMY)) {
+            game.addScore(1)
+            SHOOT.delete()
+            ENEMY.delete()
+        }
+        
+    }
+    if (SHOOT.get(LedSpriteProperty.Y) <= 0) {
+        SHOOT.delete()
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    SHIP.move(1)
+})
+let ENEMY : game.LedSprite = null
+let SHOOT : game.LedSprite = null
+let SHIP : game.LedSprite = null
+SHIP = game.createSprite(4, 4)
+game.setScore(0)
+basic.forever(function on_forever() {
+    
+    ENEMY = game.createSprite(randint(0, 10), -4)
+    ENEMY.set(LedSpriteProperty.Brightness, 150)
+    basic.pause(10)
+    ENEMY.turn(Direction.Right, 90)
+    for (let index2 = 0; index2 < 4; index2++) {
+        ENEMY.move(1)
+        basic.pause(500)
+        if (ENEMY.isTouching(SHIP)) {
+            game.gameOver()
+        }
+        
+    }
+    if (ENEMY.isTouchingEdge()) {
+        ENEMY.delete()
+    }
+    
+})
